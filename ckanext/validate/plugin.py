26558dbde527e96f8bec91cfc9be3864d5dfdb30
German Favaro <germankay@gmail.com>
2026-03-12 14:19:39 -0300

    Add scheming fields for resource validation
    - add validation_status field to resource schema
    - add validation status display snippet
    - register plugin changes required for validation UI

--- ckanext/validate/plugin.py
+++ ckanext/validate/plugin.py
@@ -1,10 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckanext.scheming.plugins import ISchemingDatasets
 
 
 class ValidatePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    
+    plugins.implements(ISchemingDatasets)
 
     # IConfigurer
 
@@ -13,4 +14,11 @@ class ValidatePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "validate")
 
-    
+    # ISchemingDatasets
+
+    def dataset_schema(self):
+        return None
+
+    def dataset_schemas(self):
+        return ["ckanext.validate:schemas/dataset.yaml"]
+
