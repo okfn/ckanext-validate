acab6f58990c35b62b6322a2536aa40f7cb9f12a
German Favaro <germankay@gmail.com>
2026-03-13 13:33:25 -0300

    flaek 8

--- ckanext/validate/tests/test_plugin.py
+++ ckanext/validate/tests/test_plugin.py
@@ -47,7 +47,8 @@ To temporary patch the CKAN configuration for the duration of a test you can use
     def test_some_action():
         pass
 """
-import ckanext.validate.plugin as plugin
+import pytest
+from ckan.plugins import plugin_loaded
 
 
 @pytest.mark.ckan_config("ckan.plugins", "validate")
